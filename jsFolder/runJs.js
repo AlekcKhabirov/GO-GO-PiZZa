fa98039a9a5fb906124e3ceb3cb126203b933d5c
Alekc <khabirov_alekc@mail.ru>
2024-03-30 23:11:12 +0300

    rendered

--- jsFolder/runJs.js
+++ jsFolder/runJs.js
@@ -19,24 +19,82 @@ const toppingsToogle=()=> {
         }
     } );     
 }
-
+/*       3    */
 const getPizzas= async()=>{
     try {
-        const response = await fetch(`https://shrub-pineapple-rosemary.glitch.me/api/products`); 
-    } catch (error) {
-        console.error(`error fetching error products:, ${error}`);
+        const response = await fetch(`https://shrub-pineapple-rosemary.glitch.me/api/products`);
+        if (!response.ok) {
+            throw new Error('failed you fetch pizza products');      
+        } 
+        return await response.json();/*       4    */
+       /* const data= await response.json();
+        console.log('data:',data);*/
+    } catch (erorr) {
+        console.error(`error fetching error pizza products:, ${error}`);
     }
+
+        
+    
     
 }
- 
-const renderPizzas=()=>{
-    const pizzas=getPizzas();
-    console.log('pizzas:',pizzas);
+ /*    2       */
+const  renderPizzas=async()=>{
+    const pizzas = await  getPizzas();/*    5       */
+   /* console.log('pizzas:',pizzas);*/
+    const pizzaLIst= document.querySelector('.pizza__list');
+    
+   /* console.log(pizzaLIst);           */
+    pizzaLIst.textContent="";
+
+    const createCard=(data)=>{
+        const card =document.createElement('article');
+        card.classList.add('card', 'pizza__card');
+        card.innerHTML=
+        `
+        <picture>
+        <source srcset="${data.images[1]}" type="image/webp">
+        <img class="card__image" src="${data.images[0]}" alt="${data.name.ru}" >
+        </picture>
+       
+        <div class="card__content">
+                       
+            <h3 class="card__title">${data.name['ru']}</h3>          
+
+            <div class="card__info">
+                <span class="card__price">${data.price["30cm"]} р</span>
+
+                <span >/</span>
+
+                <span class="card__weigth">30см.</span>
+            </div>
+    
+            <button class="card__button" data-id="${data.id}">Выбрать</button> 
+         </div>
+        
+        `;
+        return card;
+    };
+/*      6     */
+    const items =pizzas.map((data)=>{
+        console.log('data:',data);
+       /* console.log('index:',index);,index,arr
+        console.log('arr:',arr);*/
+
+        const item =document.createElement('li');
+        item.classList.add('pizza__item');
+        /*item.textContent=data.name.en;           */
+
+        const card =createCard(data);
+        item.append(card);
+        return item;
+    })
+   pizzaLIst.append(...items);   
+   pizzaLIst.append(...card);/*       7    */
 }
 
 const init=()=>{
     toppingsToogle();
-    renderPizzas();
+    renderPizzas();/*    1       */
 }
 
-init();
+init();/*           */ /* можно через точку          */
